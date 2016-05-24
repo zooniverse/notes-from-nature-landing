7b0700056eb5b4e72bbb6928bd78a8e8fd25fc98
rafe <raphael.lafrance@gmail.com>
2016-05-24 17:07:35 -0400

    Make header a const

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -1,39 +1,36 @@
-import React, { PropTypes, Component } from 'react';
+import React, { PropTypes } from 'react';
 import { Link } from 'react-router';
 import { connect } from 'react-redux';
 import HeaderAuth from 'header/header-auth';
 
-class Header extends Component {
-  render() {
-    const { active, inactiveWorkflows, user } = this.props;
-    const activeStyle = { color: '#96f132' };
-    // ? <Link activeStyle={{ color: activeStyle }} to="/field-book">Field Book</Link>
-    return (
-      <div className={ `landing-header ${active === 'landing' ? '' : 'opaque'}` }>
-        <div className="header-links">
-          <Link className={'first'} activeStyle={ activeStyle } to="/">Get Started</Link>
-          <Link activeStyle={ activeStyle } to="/about">About</Link>
-          <a href="https://www.zooniverse.org/projects/zooniverse/notes-from-nature-relaunch/talk">
-            Talk
-          </a>
-          <a href="https://blog.notesfromnature.org/" target="_blank">Blog</a>
-          {inactiveWorkflows.length
-            ? <Link activeStyle={ activeStyle } to="/completed-expeditions" className="wide">
-                Completed Expeditions
-              </Link>
-            : ''
-          }
-          {user
-            ? ''
-            : ''
-          }
-          <div className="last">&nbsp;</div>
-        </div>
-        <HeaderAuth />
+const Header = ({ active, inactiveWorkflows, user }) => {
+  const activeStyle = { color: '#96f132' };
+  // ? <Link activeStyle={{ color: activeStyle }} to="/field-book">Field Book</Link>
+  return (
+    <div className={ `landing-header ${active === 'landing' ? '' : 'opaque'}` }>
+      <div className="header-links">
+        <Link className={'first'} activeStyle={ activeStyle } to="/">Get Started</Link>
+        <Link activeStyle={ activeStyle } to="/about">About</Link>
+        <a href="https://www.zooniverse.org/projects/zooniverse/notes-from-nature-relaunch/talk">
+          Talk
+        </a>
+        <a href="https://blog.notesfromnature.org/" target="_blank">Blog</a>
+        {inactiveWorkflows.length
+          ? <Link activeStyle={ activeStyle } to="/completed-expeditions" className="wide">
+              Completed Expeditions
+            </Link>
+          : ''
+        }
+        {user
+          ? ''
+          : ''
+        }
+        <div className="last">&nbsp;</div>
       </div>
-    );
-  }
-}
+      <HeaderAuth />
+    </div>
+  );
+};
 
 Header.propTypes = {
   dispatch: PropTypes.func,
