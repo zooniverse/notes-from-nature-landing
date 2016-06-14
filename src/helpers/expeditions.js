2af6872f760e7ed4ddca2b1db689e56649a28090
rafe <raphael.lafrance@gmail.com>
2016-06-14 18:36:43 -0400

    Change .map to .forEach so that we can append to a list

--- src/helpers/expeditions.js
+++ src/helpers/expeditions.js
@@ -10,17 +10,18 @@ export const expeditionsInGroup = (group, allWorkflows) =>
   allWorkflows.filter(e => e.display_name.startsWith(group));
 
 export function recentExpeditions(allWorkflows, classifications) {
-  const recent = classifications.map(c => c.links.workflow)
-    .filter((v, i, self) => self.indexOf(v) === i)
-    .map(id => {
-      const workflow = allWorkflows.find(w => w.id === id);
-      if (workflow) {
-        const expedition = findExpedition(workflow.display_name);
-        expedition.id = workflow.id;
-        expedition.active = workflow.active;
-        return expedition;
-      }
-    });
+  const ids = classifications.map(c => c.links.workflow)
+    .filter((v, i, self) => self.indexOf(v) === i);
+  const recent = [];
+  ids.forEach(id => {
+    const workflow = allWorkflows.find(w => w.id === id);
+    if (workflow) {
+      const expedition = findExpedition(workflow.display_name);
+      expedition.id = workflow.id;
+      expedition.active = workflow.active;
+      recent.push(expedition);
+    }
+  });
   if (recent.length > RECENT_EXPEDITIONS_LENGTH) { recent.length = RECENT_EXPEDITIONS_LENGTH; }
   return recent;
 }
