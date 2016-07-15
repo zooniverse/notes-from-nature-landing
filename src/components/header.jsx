5c15daabbdd6604e52e3d77e3af0409c6c3e04e9
mcbouslog <mcbouslog@gmail.com>
2016-07-15 15:07:15 -0500

    add stats page link back to header

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -5,9 +5,6 @@ import HeaderAuth from 'header/header-auth';
 
 const activeStyle = { color: '#96f132' };
 
-// Not including stats page until working
-// <a href="https://www.zooniverse.org/projects/zooniverse/notes-from-nature/stats" target="_blank">Statistics</a>
-
 const Header = ({ active, inactiveWorkflows, user }) =>
   <div className={ `landing-header ${active === 'landing' ? '' : 'opaque'}` }>
     <div className="header-links">
@@ -19,6 +16,7 @@ const Header = ({ active, inactiveWorkflows, user }) =>
         Talk
       </a>
       <a href="https://blog.notesfromnature.org/" target="_blank">Blog</a>
+      <a href="https://www.zooniverse.org/projects/zooniverse/notes-from-nature/stats" target="_blank">Statistics</a>
       {inactiveWorkflows.length
         ? <Link activeStyle={ activeStyle } to="/completed-expeditions" className="wide">
             Completed Expeditions
